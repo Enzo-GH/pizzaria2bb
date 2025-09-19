a1fb480aa530e7034c2a77b14eb2d1f0f401d82d
Enzo-GH <rm94182@estudante.fieb.edu.br>
2025-09-19 11:52:19 -0300

    json

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,15 +1,19 @@
+// importação da ferramenta de busca de rotas
 import axios from 'axios'
+// importação das hooks do react - estado e renderização
 import { useEffect, useState } from 'react';
 
 const Produtos = () => {
-    const [pizzas, setPizzas] = useState([])
 
+    // estado com a lista de pizzas
+    const [pizzas, setPizzas] = useState(['calabresa', 'peperoni', 'quatro queijos'])
+
+    // consulta das pizzas via rota de consumo
     axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
     .then(response=>setPizzas(response.data.data))
 
     // Mapeamento das pizzas da lista (iteração)
-    const listaPizzas = pizzas.map(pizza =>
-                    <li key={pizza.id}>{pizza.nome}</li>);
+    const listaPizzas = pizzas.map(pizza =><li>{pizza.nome}</li>);
     return(
     <>
         <h3>Listagem de Produtos</h3>
